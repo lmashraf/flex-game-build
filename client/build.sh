d54e9b7639d48c0972d06bbf530452e95cab0ddd
Mohammed Achraf Lebrazi <mohammedachraf.lebrazi@here.com>
2022-10-31 10:59:23 +0100

    CLIENT: Updated build script to copy shared resources to server

--- client/build.sh
+++ client/build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-mkdir ./generated
-echo "Created client/generated/ folder."
+export SHARED_RESOURCE="../shared/resources/levels/requiredScores.json"
+export CLIENT_RESOURCE_TARGET_DIR="generated"
+export SERVER_RESOURCE_TARGET_DIR="../server/web/"
 
-cp -v ../shared/resources/levels/requiredScores.json ./generated
-echo "Copied requiredScores.json file."
+# Create 'generated' folder
+mkdir -p ./${CLIENT_RESOURCE_TARGET_DIR}
+echo "Created client/${CLIENT_RESOURCE_TARGET_DIR}/ folder."
 
-echo "Packaging source to target/WorkClient.swf..."
+# Copy shared resources to 'generated' folder
+cp -v  ${SHARED_RESOURCE} ${CLIENT_RESOURCE_TARGET_DIR}
+echo "Copied shared resources to ${CLIENT_RESOURCE_TARGET_DIR}."
+
+# Build SWF file
+echo "Packaging source to target/WorkClient-{version_number}.swf..."
 mvn clean && mvn package
 
-# Clean
-rm -rf ./generated
+# Copy file to the server's dependency while renaming it
+mv -v $(find . | grep .swf) ${SERVER_RESOURCE_TARGET_DIR}\WorkClient.swf
+echo "Copied WorkClient to ${SERVER_RESOURCE_TARGET_DIR}"
